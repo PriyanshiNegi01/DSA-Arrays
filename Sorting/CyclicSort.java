a307d39df1a5a1f11b2a9781241918de28f6bd9c
Priyanshi Negi <121029180+PriyanshiNegi01@users.noreply.github.com>
2024-10-07 21:35:51 +0530

    feat: Cyclic Sort

---
+++ Sorting/CyclicSort.java
@@ -0,0 +1,30 @@
+package com.priyanshi.Sorting;
+import java.util.Arrays;
+
+public class CyclicSort {
+
+    public static void main(String[] args){
+        int[] arr = {3, 5, 2, 1, 4};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    // Cyclic sort code for range [0, N]
+    static void sort(int[] arr){
+        int i = 0;
+        while (i < arr.length) {
+            int correctIndex = arr[i] - 1; // index = value - 1
+            if (arr[i] != arr[correctIndex]){
+                swap(arr, i, correctIndex);
+            } else{
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
